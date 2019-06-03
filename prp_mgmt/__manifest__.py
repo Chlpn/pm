658cdebb84dfddf6bda7fe521148ae3940fea333
Chlpn <prem@isi.ae>
2019-06-03 10:39:10 +0400

    03/06/2019 - commit

---
+++ prp_mgmt/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Property Management',
+    'version': '1.0',
+    'category': 'Sales',
+    'sequence': 14,
+    'author': "iSquare Informatics",
+    'website': 'https://www.isi.ae',
+    'summary': '',
+    'depends': ['web_readonly_bypass',],
+    'data': [
+
+
+
+            ],
+    'demo': [
+
+            ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
